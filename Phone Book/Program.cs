e9a6e18c7cec10d5dcc09195e085037d0c50bec1
Anthony <fuentesdev@gmail.com>
2024-05-06 15:10:43 -0700

    Completed part 1

--- Phone Book/Program.cs
+++ Phone Book/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿using Phone_Book;
+using Spectre.Console;
+
+
+var isAppRunning = true;
+while (isAppRunning)
+{
+    var option = AnsiConsole.Prompt(
+        new SelectionPrompt<MenuOptions>()
+        .Title("What would you like to do?")
+        .AddChoices(
+            MenuOptions.AddContact,
+            MenuOptions.DeleteContact,
+            MenuOptions.UpdateContact,
+            MenuOptions.ViewContact,
+            MenuOptions.ViewAllContacts,
+            MenuOptions.Quit));
+
+    switch(option)
+    {
+        case MenuOptions.AddContact:
+            RecordController.AddContact();
+            break;
+        case MenuOptions.DeleteContact:
+            RecordController.DeleteContact();
+            break;
+        case MenuOptions.UpdateContact:
+            RecordController.UpdateContact();
+            break;
+        case MenuOptions.ViewContact:
+            RecordController.GetContactById();
+            break;
+        case MenuOptions.ViewAllContacts:
+            RecordController.GetContacts();
+            break;
+        case MenuOptions.Quit:
+            isAppRunning = false;
+            break;
+    }
+}
+
+enum MenuOptions
+{
+    AddContact,
+    DeleteContact,
+    UpdateContact,
+    ViewContact,
+    ViewAllContacts,
+    Quit
+}
---
+++ Phone Book/RecordController.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Phone_Book;
+
+internal class RecordController
+{
+    internal static void AddContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void DeleteContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void GetContactById()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void GetContacts()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void UpdateContact()
+    {
+        throw new NotImplementedException();
+    }
+}
